52a4430385af275be59912164e79cd7bcc21e1c1
Dale <drcorns@hotmail.com>
2016-01-24 10:29:32 -0800

    add some default values

--- index.js
+++ index.js
@@ -7,29 +7,40 @@
 'use strict';
 module.exports = function(width, height){
   var menu = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  width = width || 100;
+  height = height || 25;
   menu.setAttribute('width', width);
   menu.setAttribute('height', height);
   var items = [];
   return{
     setViewBox: function setViewBox(x, y, xr, xy){
-      var vb = x || '0' + ' ' + (y || '0') + ' ' + (xr || '') + ' ' + (xy || '') + ' ';
+      var vb = x || '0' + ' ' + (y || '0') + ' ' + (xr || width) + ' ' + (xy || height);
       menu.setAttribute('viewBox', vb);
     },
     getMenu: function getMenu(){
       return menu;
     },
-    addItem: function addItem(id, txt, x, y, fill, width, height){
+    addItem: function addItem(id, txt, x, y, fill, iWidth, iHeight, txtXOffset, txtYOffset){
       var item = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
       var iText = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-      item.id = id;
+      item.id = id || '';
+      txt = txt || '';
+      x = x || 0;
+      y = y || 0;
+      fill = fill || 'transparent';
+      iWidth = iWidth || width;
+      iHeight = iHeight || height;
+      txtXOffset = txtXOffset || width/2;
+      txtYOffset = txtYOffset || 0;
       item.setAttribute('x', x);
       item.setAttribute('y', y);
       item.setAttribute('fill', fill);
-      item.setAttribute('width', width);
-      item.setAttribute('height', height);
-      iText.textContent = txt || '';
-      iText.setAttribute('x', x + 10);
-      iText.setAttribute('y', y);
+      item.setAttribute('width', iWidth);
+      item.setAttribute('height', iHeight);
+      iText.textContent = txt;
+      iText.setAttribute('x', x + txtXOffset);
+      iText.setAttribute('y', y + txtYOffset);
+      iText.setAttribute('text-anchor', 'middle');
       items.push({'item': item, 'txt': iText});
       menu.appendChild(item);
       menu.appendChild(iText);
--- test.js
+++ test.js
@@ -4,10 +4,9 @@
  * Copyright © 2016 Dale Corns
  */
 'use strict';
-var menu = require('./index.js')(120, 120);
+var menu = require('./index.js')();
 var main = document.getElementById('container');
 main.appendChild(menu.getMenu());
-menu.setViewBox(0, 0, 120, 120);
-menu.getMenu().classList.add('test');
-menu.addItem('item2', 'Click Me', 10, 10, 'red', 100, 100);
-console.dir(menu.getItems());
+//menu.setViewBox();
+//menu.getMenu().classList.add('test');
+menu.addItem('item1', 'Click Here', null, null, '#49F592', null, null, null, 18);
